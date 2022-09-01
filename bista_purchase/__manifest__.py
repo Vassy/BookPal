59503aa4bf704a56209527f4978a2fac4f904812
Bhautik Vasoya <bhautik.vasoya@bistasolutions.com>
2022-09-01 16:51:10 +0530

    [ADD]Inital Commit: Vendor fields

---
+++ bista_purchase/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bista Purchase",
+    'summary': """Purchase Customisation""",
+    'description': """Purchase Customisation""",
+    'author': "Bista Solutions Pvt Ltd.",
+    'website': "http://www.bistasolutions.com",
+    'category': "Purchase",
+    'version': '15.0.1.0.1',
+    'depends': ['purchase', 'delivery'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': True
+}
