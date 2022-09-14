60782f02611cc4a6b890d542cc5ab25961a21ece
Nikita Vaghela <nikita.vaghela@bistasolutions.com>
2022-09-14 17:02:12 +0530

    [IMP]Updated the logic for qty_delivered in shiping line.

--- bista_sale_multi_ship/models/sale_multi_ship.py
+++ bista_sale_multi_ship/models/sale_multi_ship.py
@@ -10,8 +10,9 @@ import base64
 import contextlib
 import csv
 import io
+from collections import defaultdict
 
-from odoo import _, fields, models
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 from odoo.tools import pycompat
 
@@ -53,6 +54,7 @@ class SaleMultiShip(models.Model):
         help="Split Shipping lines for different shipping addresses")
 
     def export_customer_shipping_detail_template(self):
+        """Export customer shipping detail."""
         with contextlib.closing(io.BytesIO()) as csvfile:
             filename = "Export Customer Template - %s" % (self.sale_id.name)
             writer = pycompat.csv_writer(csvfile, dialect='UNIX')
@@ -92,6 +94,7 @@ class SaleMultiShip(models.Model):
         }
 
     def import_customer_shipping_detail_template(self):
+        """Import customer shipping details."""
         context = self.env.context.copy()
         form_view_id = self.env.ref(
             'bista_sale_multi_ship.sale_order_multi_ship_form_wizard')
@@ -213,6 +216,7 @@ class SaleMultiShip(models.Model):
         }
 
     def action_validate_customer_data(self):
+        """Action validate customer data."""
         context = self.env.context.copy()
         form_view_id = self.env.ref(
             'bista_sale_multi_ship.sale_order_multi_ship_form_wizard')
@@ -250,6 +254,8 @@ class SaleMultiShipQtyLines(models.Model):
     product_uom_qty = fields.Float(
         related='so_line_id.product_uom_qty',
         string="Ordered Qty", help='Product Quantity')
+    product_uom = fields.Many2one(
+        related="so_line_id.product_uom", store=True)
     product_qty = fields.Float(string="Shipping Qty")
     personalization_1 = fields.Char('Personalization 1')
     personalization_2 = fields.Char('Personalization 2')
@@ -291,16 +297,249 @@ class SaleMultiShipQtyLines(models.Model):
         default='draft')
     partner_state = fields.Selection(
         related="partner_id.state", string='Contact status')
-    qty_delivered_method = fields.Selection(
-        related="so_line_id.qty_delivered_method")
-    qty_delivered = fields.Float(
-        related="so_line_id.qty_delivered",
-        string='Delivered Quantity',
-        store=True,
-        default=0.0)
-    qty_invoiced = fields.Float(related="so_line_id.qty_invoiced")
     display_qty_widget = fields.Boolean(
         related="so_line_id.display_qty_widget")
+    qty_delivered = fields.Float(
+        'Delivered Quantity', copy=False,
+        compute='_compute_qty_delivered',
+        inverse='_inverse_qty_delivered',
+        store=True, digits='Product Unit of Measure',
+        default=0.0)
+    qty_delivered_manual = fields.Float(
+        'Delivered Manually', copy=False,
+        digits='Product Unit of Measure', default=0.0)
+    qty_invoiced = fields.Float(related="so_line_id.qty_invoiced")
+    qty_delivered_method = fields.Selection(
+        related="so_line_id.qty_delivered_method", store=True)
+    product_type = fields.Selection(related='product_id.detailed_type')
+    virtual_available_at_date = fields.Float(
+        compute='_compute_qty_at_date', digits='Product Unit of Measure')
+    scheduled_date = fields.Datetime(compute='_compute_qty_at_date')
+    forecast_expected_date = fields.Datetime(compute='_compute_qty_at_date')
+    free_qty_today = fields.Float(
+        compute='_compute_qty_at_date', digits='Product Unit of Measure')
+    qty_available_today = fields.Float(compute='_compute_qty_at_date')
+    warehouse_id = fields.Many2one(related='order_id.warehouse_id')
+    qty_to_deliver = fields.Float(
+        compute='_compute_qty_to_deliver', digits='Product Unit of Measure')
+    # is_mto = fields.Boolean(compute='_compute_is_mto')
+    display_qty_widget = fields.Boolean(compute='_compute_qty_to_deliver')
+
+    @api.depends('product_type', 'product_uom_qty',
+                 'qty_delivered', 'state', 'move_ids', 'product_uom')
+    def _compute_qty_to_deliver(self):
+        """Compute the visibility of the inventory widget."""
+        for line in self:
+            line.qty_to_deliver = line.product_qty - line.qty_delivered
+            if line.state in ('draft', 'sent', 'sale') and \
+                    line.product_type == 'product' and \
+                    line.qty_to_deliver > 0:
+                if line.state == 'sale' and not line.move_ids:
+                    line.display_qty_widget = False
+                else:
+                    line.display_qty_widget = True
+            else:
+                line.display_qty_widget = False
+
+    @api.depends(
+        'product_id', 'product_qty', 'order_id.commitment_date',
+        'move_ids', 'move_ids.forecast_expected_date',
+        'move_ids.forecast_availability')
+    def _compute_qty_at_date(self):
+        """Compute the quantity forecasted of product at delivery date.
+
+        There are two cases:
+         1. The quotation has a commitment_date, we take it as
+            delivery date
+         2. The quotation hasn't commitment_date, we compute the
+            estimated delivery
+            date based on lead time
+        """
+        treated = self.browse()
+        # If the state is already in sale the picking is created and a
+        # simple forecasted quantity isn't enough
+        # Then used the forecasted data of the related stock.move
+        for line in self.filtered(lambda l: l.state == 'sale'):
+            if not line.display_qty_widget:
+                continue
+            moves = line.move_ids.filtered(
+                lambda m: m.product_id == line.product_id)
+            line.forecast_expected_date = max(moves.filtered(
+                "forecast_expected_date").mapped(
+                "forecast_expected_date"), default=False)
+            line.qty_available_today = 0
+            line.free_qty_today = 0
+            for move in moves:
+                line.qty_available_today += move.product_uom._compute_quantity(
+                    move.reserved_availability, line.product_uom)
+                line.free_qty_today += move.product_id.uom_id.\
+                    _compute_quantity(
+                        move.forecast_availability, line.product_uom)
+            line.virtual_available_at_date = False
+            treated |= line
+
+        qty_processed_per_product = defaultdict(lambda: 0)
+        grouped_lines = defaultdict(
+            lambda: self.env['sale.multi.ship.qty.lines'])
+        # We first loop over the SO lines to group them by
+        # warehouse and schedule
+        # date in order to batch the read of the quantities computed field.
+        for line in self.filtered(lambda l: l.state in ('draft', 'sent')):
+            if not (line.product_id and line.display_qty_widget):
+                continue
+            grouped_lines[(
+                line.warehouse_id.id, line.shipping_date)] |= line
+
+        for (warehouse, shipping_date), lines in grouped_lines.items():
+            product_qties = lines.mapped('product_id').with_context(
+                to_date=shipping_date, warehouse=warehouse).read([
+                    'qty_available',
+                    'free_qty',
+                    'virtual_available',
+                ])
+            qties_per_product = {
+                product['id']: (product['qty_available'],
+                                product['free_qty'],
+                                product['virtual_available'])
+                for product in product_qties
+            }
+            for line in lines:
+                qty_available_today, free_qty_today, \
+                    virtual_available_at_date = qties_per_product[
+                        line.product_id.id]
+                line.qty_available_today = qty_available_today - \
+                    qty_processed_per_product[line.product_id.id]
+                line.free_qty_today = free_qty_today - \
+                    qty_processed_per_product[line.product_id.id]
+                line.virtual_available_at_date = virtual_available_at_date - \
+                    qty_processed_per_product[line.product_id.id]
+                line.forecast_expected_date = False
+                product_qty = line.product_uom_qty
+                if line.product_uom and line.product_id.uom_id and \
+                        line.product_uom != line.product_id.uom_id:
+                    line.qty_available_today = line.product_id.uom_id.\
+                        _compute_quantity(
+                            line.qty_available_today, line.product_uom)
+                    line.free_qty_today = line.product_id.uom_id.\
+                        _compute_quantity(
+                            line.free_qty_today, line.product_uom)
+                    line.virtual_available_at_date = line.product_id.uom_id.\
+                        _compute_quantity(
+                            line.virtual_available_at_date, line.product_uom)
+                    product_qty = line.product_uom._compute_quantity(
+                        product_qty, line.product_id.uom_id)
+                qty_processed_per_product[line.product_id.id] += product_qty
+            treated |= lines
+        remaining = (self - treated)
+        remaining.virtual_available_at_date = False
+        remaining.forecast_expected_date = False
+        remaining.free_qty_today = False
+        remaining.qty_available_today = False
+
+    # @api.depends('product_id', 'route_id',
+    #              'order_id.warehouse_id', 'product_id.route_ids')
+    # def _compute_is_mto(self):
+    #     self.is_mto = False
+    #     for line in self:
+    #         if not line.display_qty_widget:
+    #             continue
+    #         product = line.product_id
+    #         product_routes = line.route_id or (
+    #             product.route_ids + product.categ_id.total_route_ids)
+
+    #         # Check MTO
+    #         mto_route = line.order_id.warehouse_id.mto_pull_id.route_id
+    #         if not mto_route:
+    #             try:
+    #                 mto_route = self.env['stock.warehouse']._find_global_route(
+    #                     'stock.route_warehouse0_mto', _('Make To Order'))
+    #             except UserError:
+    #                 # if route MTO not found in ir_model_data, we treat the
+    #                 # product as in MTS
+    #                 pass
+
+    #         if mto_route and mto_route in product_routes:
+    #             line.is_mto = True
+    #         else:
+    #             line.is_mto = False
+
+    def _get_outgoing_incoming_moves(self):
+        outgoing_moves = self.env['stock.move']
+        incoming_moves = self.env['stock.move']
+
+        moves = self.move_ids.filtered(
+            lambda r: r.state != 'cancel' and not
+            r.scrapped and self.product_id == r.product_id)
+        if self._context.get('accrual_entry_date'):
+            moves = moves.filtered(
+                lambda r: fields.Date.to_date(
+                    r.date) <= self._context['accrual_entry_date'])
+        for move in moves:
+            if move.location_dest_id.usage == "customer":
+                if not move.origin_returned_move_id or (
+                        move.origin_returned_move_id and move.to_refund):
+                    outgoing_moves |= move
+            elif move.location_dest_id.usage != "customer" and move.to_refund:
+                incoming_moves |= move
+            elif move.location_dest_id.usage == "supplier" and move.to_refund:
+                outgoing_moves |= move
+            elif move.location_dest_id.usage != "supplier":
+                if not move.origin_returned_move_id or (
+                        move.origin_returned_move_id and move.to_refund):
+                    incoming_moves |= move
+        return outgoing_moves, incoming_moves
+
+    @api.depends('move_ids.state',
+                 'move_ids.scrapped', 'move_ids.product_uom_qty',
+                 'move_ids.product_uom',
+                 'qty_delivered_method',
+                 'qty_delivered_manual')
+    def _compute_qty_delivered(self):
+        # Just ignore for timesheet may need to check in future.
+        # lines_by_analytic = self.filtered(
+        #     lambda sol: sol.qty_delivered_method == 'analytic')
+        # mapping = lines_by_analytic._get_delivered_quantity_by_analytic(
+        #     [('amount', '<=', 0.0)])
+        # for so_line in lines_by_analytic:
+        #     so_line.qty_delivered = mapping.get(
+        #         so_line.id or so_line._origin.id, 0.0)
+        for line in self:  # TODO: maybe one day, this should be done in SQL
+            # for performance sake
+            if line.qty_delivered_method == 'stock_move':
+                qty = 0.0
+                outgoing_moves, incoming_moves = line.\
+                    _get_outgoing_incoming_moves()
+                for move in outgoing_moves:
+                    if move.state != 'done':
+                        continue
+                    qty += move.product_uom._compute_quantity(
+                        move.product_uom_qty,
+                        line.product_uom, rounding_method='HALF-UP')
+                for move in incoming_moves:
+                    if move.state != 'done':
+                        continue
+                    qty -= move.product_uom._compute_quantity(
+                        move.product_uom_qty, line.product_uom,
+                        rounding_method='HALF-UP')
+                line.qty_delivered = qty
+            elif line.qty_delivered_method == 'manual':
+                line.qty_delivered = line.qty_delivered_manual or 0.0
+
+    @api.onchange('qty_delivered')
+    def _inverse_qty_delivered(self):
+        """Inverse qty delivered.
+
+        When writing on qty_deliveredif the value should be modify manually
+        (`qty_delivered_method` = 'manual' only),
+        then we put the value in `qty_delivered_manual`.
+        Otherwise, `qty_delivered_manual` should be False since the
+        delivered qty is automatically compute by other mecanisms.
+        """
+        for line in self:
+            if line.qty_delivered_method == 'manual':
+                line.qty_delivered_manual = line.qty_delivered
+            else:
+                line.qty_delivered_manual = 0.0
 
     def name_get(self):
         """Updated the display name."""
