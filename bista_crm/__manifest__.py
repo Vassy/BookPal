5518f109fee7d501933513f184ff4fb419473535
akshay <akshay.ransure@bistasolutions>
2022-09-16 14:58:53 +0530

    [ADD] crm fields has been Updated

---
+++ bista_crm/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bista CRM',
+    'version': '15.0.1.0.0',
+    'description': 'Manage CRM',
+    'category': 'CRM',
+    'summary': 'Manage CRM',
+    'website': 'http://www.bistasolutions.com',
+    'license': 'AGPL-3',
+    'depends': ['sale_crm'],
+    'data': [
+        # Views
+        'views/crm_lead_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
