ed2d13d0501c52bd35f3c934a4b18e9aab771940
Bhautik Vasoya <bhautik.vasoya@bistasolutions.com>
2022-09-22 11:38:02 +0530

    [IMP]Auto select pricelist on customer

--- purchase_pricelist/models/product_pricelist.py
+++ purchase_pricelist/models/product_pricelist.py
@@ -6,3 +6,8 @@ class ProductPricelist(models.Model):
     _inherit = "product.pricelist"
 
     used_for = fields.Selection([('sale',"Sale"),('purchase',"Purchase")], default='sale')
+
+    def _get_partner_pricelist_multi_search_domain_hook(self, company_id):
+        res = super(ProductPricelist, self)._get_partner_pricelist_multi_search_domain_hook(company_id)
+        res.append(('used_for', '=', 'sale'))
+        return res
