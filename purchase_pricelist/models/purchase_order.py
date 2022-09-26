5cc23902f21a466584628451c5a7b734825c25d1
Bhautik Vasoya <bhautik.vasoya@bistasolutions.com>
2022-09-26 16:50:13 +0530

    [ADD]Pricelist Based on Order

--- purchase_pricelist/models/purchase_order.py
+++ purchase_pricelist/models/purchase_order.py
@@ -5,19 +5,63 @@ from odoo import api, fields, models
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    pricelist_id = fields.Many2one('product.pricelist', "Pricelist")
+    pricelist_id = fields.Many2one('product.pricelist', "Pricelist", compute="_compute_product_pricelist", store=True)
+    without_disc_amount_untaxed = fields.Monetary(string='Without Untaxed Amount', store=True, readonly=True, compute='_amount_all')
+    total_discount_amount = fields.Monetary(string='Total Discount Amount', store=True, readonly=True, compute='_amount_all')
+
+    def _amount_all(self):
+        super(PurchaseOrder, self)._amount_all()
+        for order in self:
+            without_disc_amount_untaxed = total_discount_amount = 0.0
+            for line in order.order_line:
+                without_disc_amount_untaxed += line.without_disc_price_subtotal
+                total_discount_amount += line.discount_amount
+            currency = order.currency_id or order.partner_id.property_purchase_currency_id or self.env.company.currency_id
+            order.update({
+                'without_disc_amount_untaxed': currency.round(without_disc_amount_untaxed),
+                'total_discount_amount': currency.round(total_discount_amount),
+            })
+
+    @api.depends('partner_id')
+    def _compute_product_pricelist(self):
+        for order in self:
+            order.pricelist_id = order.partner_id.property_product_vendor_pricelist
 
     def update_prices(self):
-        pass
+        for order in self:
+            if order.pricelist_id:
+                price_list_line = order.pricelist_id.get_pricelist_order_line_based_on_amount(order.without_disc_amount_untaxed)
+                non_discounted_lines = order.order_line
+                if non_discounted_lines and price_list_line:
+                    non_discounted_lines.discount = price_list_line.discount
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
+    disc_price_unit = fields.Float(string="Disc Unit Price", compute="_compute_disc_price_unit")
+    without_disc_price_subtotal = fields.Monetary(compute='_compute_amount', string='Without Disc. Subtotal', store=True)
+    discount_amount = fields.Monetary(compute='_compute_amount', string='Discount Amount', store=True)
+
+    def _compute_disc_price_unit(self):
+        for order_line in self:
+            order_line.disc_price_unit = order_line._get_discounted_price_unit()
+
     # adding discount to depends
     @api.depends("discount")
     def _compute_amount(self):
-        return super()._compute_amount()
+        res = super()._compute_amount()
+        for line in self:
+            vals = line._prepare_compute_all_values()
+            vals.update({'price_unit': line.price_unit})
+            taxes = line.taxes_id.compute_all(**vals)
+            line.update({
+                # 'price_tax': taxes['total_included'] - taxes['total_excluded'],
+                # 'price_total': taxes['total_included'],
+                'discount_amount': line.price_subtotal - taxes['total_excluded'],
+                'without_disc_price_subtotal': taxes['total_excluded'],
+            })
+        return res
 
     def _prepare_compute_all_values(self):
         vals = super()._prepare_compute_all_values()
