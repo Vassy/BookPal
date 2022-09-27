5ad68699b34a1845a10345349fb756051f1c4520
Bhautik Vasoya <bhautik.vasoya@bistasolutions.com>
2022-09-27 17:24:17 +0530

    [ADD]Onchange of currency pricelist should be false

--- purchase_pricelist/models/res_partner.py
+++ purchase_pricelist/models/res_partner.py
@@ -9,7 +9,7 @@ class ResPartner(models.Model):
         'product.pricelist', 'Pricelist', company_dependent=False,
         help="This pricelist will be used, instead of the default one, for purchase to the current partner")
 
-    @api.onchange('country_id')
-    def onchange_country_change_pricelist(self):
-        if self.country_id.currency_id != self.property_product_vendor_pricelist.currency_id:
-            self.property_product_vendor_pricelist.currency_id = False
+    @api.onchange('property_purchase_currency_id')
+    def onchange_property_purchase_currency_id(self):
+        if self.property_purchase_currency_id != self.property_product_vendor_pricelist.currency_id:
+            self.property_product_vendor_pricelist = False
