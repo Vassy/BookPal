421c16a768e9238d406674c98a6084670ed1c268
Nikita Vaghela <nikita.vaghela@bistasolutions.com>
2022-09-27 11:53:55 +0530

    [FIX]Fixed the MTS-MTO issue when update the qty in sale order line or shipping line.

--- stock_mts_mto_rule/models/stock_rule.py
+++ stock_mts_mto_rule/models/stock_rule.py
@@ -1,6 +1,7 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import _, api, fields, models
+
 from odoo.exceptions import ValidationError
 from odoo.osv import expression
 from odoo.tools import float_compare, float_is_zero
@@ -13,8 +14,10 @@ class StockRule(models.Model):
         selection_add=[("split_procurement", "Choose between MTS and MTO")],
         ondelete={"split_procurement": "cascade"},
     )
-    mts_rule_id = fields.Many2one("stock.rule", string="MTS Rule", check_company=True)
-    mto_rule_id = fields.Many2one("stock.rule", string="MTO Rule", check_company=True)
+    mts_rule_id = fields.Many2one(
+        "stock.rule", string="MTS Rule", check_company=True)
+    mto_rule_id = fields.Many2one(
+        "stock.rule", string="MTO Rule", check_company=True)
 
     @api.constrains("action", "mts_rule_id", "mto_rule_id")
     def _check_mts_mto_rule(self):
@@ -22,13 +25,11 @@ class StockRule(models.Model):
             if rule.action == "split_procurement":
                 if not rule.mts_rule_id or not rule.mto_rule_id:
                     msg = _(
-                        "No MTS or MTO rule configured on procurement " "rule: %s!"
-                    ) % (rule.name,)
+                        "No MTS or MTO rule configured on procurement "
+                        "rule: %s!") % (rule.name,)
                     raise ValidationError(msg)
-                if (
-                    rule.mts_rule_id.location_src_id.id
-                    != rule.mto_rule_id.location_src_id.id
-                ):
+                if (rule.mts_rule_id.location_src_id.id !=
+                        rule.mto_rule_id.location_src_id.id):
                     msg = _(
                         "Inconsistency between the source locations of "
                         "the mts and mto rules linked to the procurement "
@@ -37,19 +38,32 @@ class StockRule(models.Model):
                     raise ValidationError(msg)
 
     def get_mto_qty_to_order(self, product, product_qty, product_uom, values):
+        """Updated logic when update po qty with negetive qty."""
         self.ensure_one()
         precision = self.env["decimal.precision"].precision_get(
             "Product Unit of Measure"
         )
         src_location_id = self.mts_rule_id.location_src_id.id
+        # find the reserved moves and update the prodcut_qty."""
+        dom = [('product_id', '=', product.id),
+               ('location_id', '=', src_location_id),
+               ('date', '=', values.get('date_planned')),
+               ('date_deadline', '=', values.get('date_planned')),
+               ('state', '=', 'assigned')]
+        reserved_moves = self.env['stock.move'].sudo().search(dom)
+        if reserved_moves and product_qty < 0:
+            # reserved_moves._do_unreserve()
+            for rmv in reserved_moves:
+                product_qty += rmv.product_uom_qty
+
         product_location = product.with_context(location=src_location_id)
         virtual_available = product_location.virtual_available
-        qty_available = product.uom_id._compute_quantity(virtual_available, product_uom)
+        qty_available = product.uom_id._compute_quantity(
+            virtual_available, product_uom)
         if float_compare(qty_available, 0.0, precision_digits=precision) > 0:
-            if (
-                float_compare(qty_available, product_qty, precision_digits=precision)
-                >= 0
-            ):
+            if (float_compare(
+                    qty_available, product_qty,
+                    precision_digits=precision) >= 0):
                 return 0.0
             else:
                 return product_qty - qty_available
@@ -70,27 +84,26 @@ class StockRule(models.Model):
                 procurement.values,
             )
             if float_is_zero(needed_qty, precision_digits=precision):
-                getattr(self.env["stock.rule"], "_run_%s" % rule.mts_rule_id.action)(
+                getattr(self.env["stock.rule"], "_run_%s" %
+                        rule.mts_rule_id.action)(
                     [(procurement, rule.mts_rule_id)]
                 )
-            elif (
-                float_compare(
-                    needed_qty, procurement.product_qty, precision_digits=precision
-                )
-                == 0.0
-            ):
-                getattr(self.env["stock.rule"], "_run_%s" % rule.mto_rule_id.action)(
+            elif (float_compare(
+                    needed_qty, procurement.product_qty,
+                    precision_digits=precision) == 0.0):
+                getattr(self.env["stock.rule"], "_run_%s" %
+                        rule.mto_rule_id.action)(
                     [(procurement, rule.mto_rule_id)]
                 )
             else:
                 mts_qty = procurement.product_qty - needed_qty
                 mts_procurement = procurement._replace(product_qty=mts_qty)
-                getattr(self.env["stock.rule"], "_run_%s" % rule.mts_rule_id.action)(
+                getattr(self.env["stock.rule"], "_run_%s" %
+                        rule.mts_rule_id.action)(
                     [(mts_procurement, rule.mts_rule_id)]
                 )
-
-                # Search all confirmed stock_moves of mts_procuremet and assign them
-                # to adjust the product's free qty
+                # Search all confirmed stock_moves of mts_procuremet and
+                # assign the to adjust the product's free qty
                 group_id = mts_procurement.values.get("group_id")
                 group_domain = expression.AND(
                     [domain, [("group_id", "=", group_id.id)]]
@@ -101,7 +114,8 @@ class StockRule(models.Model):
                 moves_to_assign._action_assign()
 
                 mto_procurement = procurement._replace(product_qty=needed_qty)
-                getattr(self.env["stock.rule"], "_run_%s" % rule.mto_rule_id.action)(
+                getattr(self.env["stock.rule"], "_run_%s" %
+                        rule.mto_rule_id.action)(
                     [(mto_procurement, rule.mto_rule_id)]
                 )
         return True
