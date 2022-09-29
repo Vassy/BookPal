9974fd2a5799aeef33343aa69605ef09197980a2
Bhautik Vasoya <bhautik.vasoya@bistasolutions.com>
2022-09-29 12:22:01 +0530

    [IMP]Change currency warning message changes

--- purchase_pricelist/models/res_partner.py
+++ purchase_pricelist/models/res_partner.py
@@ -16,6 +16,6 @@ class ResPartner(models.Model):
             return {
                 'warning': {
                     'title': "Vendor Pricelist Change",
-                    'message':"Vendor pricelist have been reset due to currency change."
+                    'message':"Vendor pricelist need to reset due to change in currency."
                 }
             }
