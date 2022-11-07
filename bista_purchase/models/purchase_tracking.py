1837d5974ce5bc1d9980ab32b8695cab44808574
Bhautik Vasoya <bhautik.vasoya@bistasolutions.com>
2022-11-07 11:28:14 +0530

    [ADD]Purchase Tacking Update

--- bista_purchase/models/purchase_tracking.py
+++ bista_purchase/models/purchase_tracking.py
@@ -17,12 +17,16 @@ class PurchaseTracking(models.Model):
     carrier_id = fields.Many2one('delivery.carrier', "Carrier")
     tracking_ref = fields.Char('Tracking Ref.', tracking=True)
     shipment_date = fields.Date(string="Shipment Date", tracking=True)
-    shipment_name = fields.Char('Shipment Name', tracking=True)
+    pro_number = fields.Char('Pro Number', tracking=True)
     tracking_line_ids = fields.One2many('purchase.tracking.line', 'tracking_id', string="Tracking Lines")
     status = fields.Selection([('draft', 'Draft'),
                                ('pending', 'Pending/In Transint'),
                                ('received', 'Received'),
                                ('on_hold', 'On Hold')], default='draft', tracking=True)
+    tracking_ref_ids = fields.One2many('purchase.tracking.ref', 'purchase_tracking_id', string="Tracking Refs")
+
+    def save(self):
+        return self
 
     @api.onchange('status')
     def onchange_status(self):
@@ -46,12 +50,34 @@ class PurchaseTracking(models.Model):
         return defaults
 
 
+class PurchaseTrackingRef(models.Model):
+    _name = 'purchase.tracking.ref'
+
+    purchase_tracking_id = fields.Many2one('purchase.tracking', '')
+    name = fields.Char(string='Name')
+    link = fields.Char(string="Link", compute="compute_link_from_ref")
+
+    @api.depends('name')
+    def compute_link_from_ref(self):
+        for each in self:
+            each.link = each.name
+
 class PurchaseTrackingLine(models.Model):
     _name = 'purchase.tracking.line'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Purchase Tracking Line'
 
+    checkbox = fields.Boolean(string="Checkbox", compute="compute_checkbox")
     tracking_id = fields.Many2one('purchase.tracking', string="Tracking")
     po_line_id = fields.Many2one('purchase.order.line', 'PO Line')
+    order_qty = fields.Float(related='po_line_id.product_qty', string="Ordered Quantity")
     product_id = fields.Many2one('product.product', 'Product')
-    ship_qty = fields.Float(string="Ship Quantity")
+    ship_qty = fields.Float(string="Shipped Quantity")
+
+    def compute_checkbox(self):
+        for each in self:
+            each.checkbox = False
+
+    @api.onchange('checkbox')
+    def _onchange_checkbox(self):
+        self.ship_qty = self.order_qty
