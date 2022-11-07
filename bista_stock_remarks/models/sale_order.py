340051f9fb826fe888d89a42414eb88264959f96
Akhtar-Bista <akhtar.khan@bistasolutions.com>
2022-11-07 15:09:15 +0600

    [Add] bista_stock_remarks module

--- bista_stock_remarks/models/sale_order.py
+++ bista_stock_remarks/models/sale_order.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import  _,api,fields, models
-
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
@@ -9,25 +8,23 @@ class SaleOrder(models.Model):
     common_pick_note = fields.Html('Common Notes',)
 
 
-
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     picking_note = fields.Text('Picking Note')
 
     def _prepare_procurement_values(self, group_id=False):
-        values = super(SaleOrderLine, self)._prepare_procurement_values(group_id)
+        values = super(SaleOrderLine, self)._prepare_procurement_values(
+            group_id)
         pick_note = {
             'picking_note': self.picking_note
-            }
+        }
         if values and type(values) is list:
             for rec in values:
-                pick = rec['ship_line']
-                if pick:
-                    pick_note = {
-                        'picking_note': pick.picking_note
-                        }
-                rec.update(pick_note)
+                shipping_line = rec['ship_line']
+                if shipping_line:
+                    rec.update(
+                        {'picking_note': shipping_line.picking_note})
         elif values and type(values) is dict:
             values.update(pick_note)
         return values
@@ -36,7 +33,6 @@ class SaleOrderLine(models.Model):
 class SaleMultiShipQtyLines(models.Model):
     _inherit = "sale.multi.ship.qty.lines"
     _description = 'Sale Multi Ship Qty Lines model details.'
-
 
     picking_note = fields.Text('Picking Note')
 
