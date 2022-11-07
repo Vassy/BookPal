596913815504dc90dd0eb436135a1baf01d2ebcc
Akhtar-Bista <akhtar.khan@bistasolutions.com>
2022-11-07 13:55:03 +0600

    Commented code for remarks field functionality

--- bista_sale/models/__init__.py
+++ bista_sale/models/__init__.py
@@ -1,6 +1,6 @@
 from . import sale_order
 from . import stock_move
-from . import stock_rule
+# from . import stock_rule
 from . import white_glove_type
 from . import artwork_status
 from . import death_type
