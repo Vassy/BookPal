c5d101e0dacbb8a61d080af19a31973218d86afb
Annapurna Prajapati <annapurna.prajapati@bista-it-00217.bistasolutions.com>
2022-11-08 15:05:23 +0530

    [ADD] Added bista contact module for customer fields.

---
+++ bista_contact/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bista Contact',
+    'version': '15.0.1.0.0',
+    'description': 'Manage Conatct',
+    'category': 'Contact',
+    'summary': 'Manage Conatct',
+    'website': 'http://www.bistasolutions.com',
+    'license': 'AGPL-3',
+    'depends': ['base','product'],
+    'data': [
+
+        'views/res_partner_views.xml',
+
+    ],
+    'installable': True,
+    'auto_install': False
+}
