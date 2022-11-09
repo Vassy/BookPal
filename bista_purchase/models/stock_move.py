be0153d468868b3862d730faa02c0a1f86c2255a
Bhautik Vasoya <bhautik.vasoya@bistasolutions.com>
2022-11-09 15:38:52 +0530

    [ADD]PO Lines status: Stocked

--- bista_purchase/models/stock_move.py
+++ bista_purchase/models/stock_move.py
@@ -11,9 +11,14 @@ class StockMove(models.Model):
         if vals.get('state') == 'done':
             received_status_id = self.env.ref('bista_purchase.status_line_received')
             partially_received_status_id = self.env.ref('bista_purchase.status_line_partially_received')
+            stocked_status_id = self.env.ref('bista_purchase.status_line_stocked')
+            input_location_id = self.env.ref('stock.stock_location_company')
+            stock_location_id = self.env.ref('stock.stock_location_stock')
             for move in self:
-                if move.state == 'done' and move.purchase_line_id:
+                if move.state == 'done' and move.purchase_line_id and move.location_dest_id == input_location_id:
                     # purchase_quantity_received = self.purchase_line_id.qty_received + self.quantity_done
                     purchase_quantity_received = move.purchase_line_id.qty_received
                     move.purchase_line_id.status_id = received_status_id.id if move.purchase_line_id.product_qty == purchase_quantity_received else partially_received_status_id.id
+                if move.state == 'done' and move.move_orig_ids.mapped('purchase_line_id') and move.location_dest_id == stock_location_id:
+                    move.move_orig_ids.mapped('purchase_line_id').status_id = stocked_status_id.id
         return res
