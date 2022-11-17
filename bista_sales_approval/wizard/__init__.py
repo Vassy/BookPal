2c43d096e85acd8b42190511a9cb1a60b9b9c7c0
Mukul Roy <mukul.roy@bistasolutions.com>
2022-11-17 19:50:26 +0530

    [IMP] optimize the code of bista_sales_approval module

--- bista_sales_approval/wizard/__init__.py
+++ bista_sales_approval/wizard/__init__.py
@@ -1,4 +1,10 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+# Bista Solutions Pvt. Ltd
+# Copyright (C) 2022 (https://www.bistasolutions.com)
+#
+##############################################################################
 
 from . import so_reject_reason
 from . import min_price_wizard
