3ab973d3cc91d5dfea7d1d0e7b2c3e80d21e4892
Mukul Roy <mukul.roy@bistasolutions.com>
2022-11-29 18:15:42 +0530

    [IMP] Bug #17335 solved pricelist issue at purchase line level

--- purchase_pricelist/models/res_partner.py
+++ purchase_pricelist/models/res_partner.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from odoo import models, api, fields
 
 
@@ -6,16 +7,18 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     property_product_vendor_pricelist = fields.Many2one(
-        'product.pricelist', 'Pricelist', company_dependent=False,
-        help="This pricelist will be used, instead of the default one, for purchase to the current partner")
+        "product.pricelist",
+        help="This pricelist will be used, instead of the default one, "
+        "for purchase to the current partner",
+    )
 
-    @api.onchange('property_purchase_currency_id')
+    @api.onchange("property_purchase_currency_id")
     def onchange_property_purchase_currency_id(self):
-        if self.property_purchase_currency_id != self.property_product_vendor_pricelist.currency_id and self.property_product_vendor_pricelist:
+        if (
+            self.property_purchase_currency_id
+            != self.property_product_vendor_pricelist.currency_id
+            and self.property_product_vendor_pricelist
+        ):
             self.property_product_vendor_pricelist = False
-            return {
-                'warning': {
-                    'title': "Vendor Pricelist Change",
-                    'message':"Vendor pricelist needs to reset due to change in currency."
-                }
-            }
+            msg = "Vendor pricelist needs to reset due to change in currency."
+            return {"warning": {"title": "Vendor Pricelist Change", "message": msg}}
