0b4dc360ec28398e3340f2862e2461081799a9cf
Nikita Vaghela <nikita.vaghela@bistasolutions.com>
2022-12-07 16:40:01 +0530

    [IMP]Improved the bista_stock_remark manifest.

--- bista_stock_remarks/__manifest__.py
+++ bista_stock_remarks/__manifest__.py
@@ -1,7 +1,8 @@
 {
     'name': 'Bista Stock Remarks',
     'version': '15.0.1.0.0',
-    'description': 'Manage Sale order and Purchase Order custom field "Remark" to stock',
+    'description': 'Manage Sale order and Purchase Order custom'
+    'field "Remark" to stock',
     'category': '',
     'summary': '',
     'website': 'http://www.bistasolutions.com',
