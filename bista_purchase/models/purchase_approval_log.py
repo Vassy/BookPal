11e36b63bb26562f61f3c59752c23f7fd0ea3cd4
Unnati Bhavsar <unnati.bhavsar@bista-it-00241.bistasolutions.com>
2022-12-14 13:03:35 +0530

    [ADD] - Purchase Order Approve functionality

---
+++ bista_purchase/models/purchase_approval_log.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class PurchaseApprovalLog(models.Model):
+    _name = "purchase.approval.log"
+    _description = "Log information of Purchase Order Approval Process"
+
+    order_id = fields.Many2one("purchase.order", ondelete="cascade")
+    note = fields.Text(string="Reason")
+    done_action = fields.Char(string="Performed Action")
+    action_user_id = fields.Many2one(
+        "res.users", string="User", default=lambda self: self.env.uid
+    )
+    action_date = fields.Datetime(string="Date", default=fields.Datetime.now)
+    old_state = fields.Char(string='Old State')
+    state = fields.Char(string='State')
--- bista_purchase/wizard/__init__.py
+++ bista_purchase/wizard/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import update_shipment_tracking
 from . import stock_picking_return
+from . import order_reject
