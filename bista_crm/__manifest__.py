64a2fe8446f68dff16aa9894161071944e307aa6
Akshay Ransure <akshay.ransure@bista-it-00215.bistasolutions.com>
2022-12-15 13:41:00 +0530

    [IMP] Fields added and removed in as given sheet,resolved Feature #17871,Bug #18132,Bug #18437,Feature #18074,Bug #18436

--- bista_crm/__manifest__.py
+++ bista_crm/__manifest__.py
@@ -7,14 +7,15 @@
     'summary': 'Manage CRM',
     'website': 'http://www.bistasolutions.com',
     'license': 'AGPL-3',
-    'depends': ['sale_crm', 'bista_sale'],
+    'depends': ['sale_crm', 'bista_sale','utm'],
     'data': [
         # Views
         'security/ir.model.access.csv',
         'views/configuration_view.xml',
         'views/crm_lead_views.xml',
         'data/shipping_price_type_data.xml',
-        'data/fullfillment_warehouse_data.xml'
+        'data/fullfillment_warehouse_data.xml',
+        'data/deal_source_data.xml'
     ],
     'installable': True,
     'auto_install': False
