fc915204ece87bbc4d8b6df24eb18788046307c2
Mukul Roy <mukul.roy@bistasolutions.com>
2023-01-02 17:03:05 +0530

    [IMP] make appropriate validation for cancel transfers
    (cherry picked from commit 270252f0afffea1c6e66a385cdc3e33a192c8460)

--- bista_cancel_stock/models/stock_move.py
+++ bista_cancel_stock/models/stock_move.py
@@ -9,15 +9,13 @@ class StockMove(models.Model):
 
     def _action_cancel(self):
         # validation for multi step transfer which is depends on other transfer
-        if any(
-            "done" in move.move_dest_ids.mapped("state") and move.state == "done"
-            for move in self
-        ):
-            msg = _(
-                "Delivery Is Already done for this Picking. "
-                "Cancel Delivery First and Then Picking."
-            )
-            raise ValidationError(msg)
+        for move in self.filtered(lambda m: m.state == "done"):
+            if "done" in move.move_dest_ids.mapped("state"):
+                msg = _(
+                    "You need to cancel first '%s', Then you can cancel this transfer."
+                    % ", ".join(move.move_dest_ids.mapped("picking_id.name"))
+                )
+                raise ValidationError(msg)
         self.mapped("move_line_ids").write({"state": "draft", "qty_done": 0})
         self.write({"state": "draft"})
         for account in self.mapped("account_move_ids"):
