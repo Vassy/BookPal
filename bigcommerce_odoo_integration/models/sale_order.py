fd55a3c85352b3d4d10d0205662680367d982241
Mukul Roy <mukul.roy@bistasolutions.com>
2023-01-11 17:24:47 +0530

    [IMP] Bug #18822 - comment code to solved issue of date order not changing

--- bigcommerce_odoo_integration/models/sale_order.py
+++ bigcommerce_odoo_integration/models/sale_order.py
@@ -921,21 +921,22 @@ class SaleOrderLineVts(models.Model):
     #             'price_subtotal': taxes['total_excluded'],
     #         })
 
-def action_confirm(self):
-    if self._get_forbidden_state_confirm() & set(self.mapped('state')):
-        raise UserError(_(
-            'It is not allowed to confirm an order in the following states: %s'
-        ) % (', '.join(self._get_forbidden_state_confirm())))
+# Bug #18822:- Comment code due issue of order date not changes for normal sales quote/order
+# def action_confirm(self):
+#     if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+#         raise UserError(_(
+#             'It is not allowed to confirm an order in the following states: %s'
+#         ) % (', '.join(self._get_forbidden_state_confirm())))
 
-    for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-        order.message_subscribe([order.partner_id.id])
-    self.write({
-        'state': 'sale',
-        #'date_order': fields.Datetime.now()
-    })
-    self._action_confirm()
-    if self.env.user.has_group('sale.group_auto_done_setting'):
-        self.action_done()
-    return True
+#     for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+#         order.message_subscribe([order.partner_id.id])
+#     self.write({
+#         'state': 'sale',
+#         #'date_order': fields.Datetime.now()
+#     })
+#     self._action_confirm()
+#     if self.env.user.has_group('sale.group_auto_done_setting'):
+#         self.action_done()
+#     return True
 
-SaleOrder.action_confirm = action_confirm
+# SaleOrder.action_confirm = action_confirm
