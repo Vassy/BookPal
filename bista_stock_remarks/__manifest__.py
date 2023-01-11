b0784ff475ffc031187971f0ecea8b156c9946bd
Nikita Vaghela <nikita.vaghela@bistasolutions.com>
2023-01-11 18:24:34 +0530

    [IMP]Improved Feature #18838 - change shipping instruction positiona and make required internal note and shipping note.

--- bista_stock_remarks/__manifest__.py
+++ bista_stock_remarks/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Bista Stock Remarks',
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.1.0',
     'description': 'Manage Sale order and Purchase Order custom'
     'field "Remark" to stock',
     'category': '',
