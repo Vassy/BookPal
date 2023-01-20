1460b8056a8ec64b1a2bbe64c7f8c1fc15d78701
Mukul Roy <mukul.roy@bistasolutions.com>
2023-01-20 18:22:12 +0530

    [IMP] Feature #18622 - link sale order and payments
    (cherry picked from commit 6f5b7b218d5f6a4ac1125cde5cb89ddc18a4646b)

--- sale_advance_payment/models/payment.py
+++ sale_advance_payment/models/payment.py
@@ -11,3 +11,13 @@ class AccountPayment(models.Model):
     sale_id = fields.Many2one(
         "sale.order", "Sale", readonly=True, states={"draft": [("readonly", False)]}
     )
+
+
+class PaymentTransaction(models.Model):
+    _inherit = "payment.transaction"
+
+    def _create_payment(self, **extra_create_values):
+        payment = super()._create_payment(**extra_create_values)
+        if self.sale_order_ids:
+            payment.write({"sale_id": self.sale_order_ids.ids[0]})
+        return payment
