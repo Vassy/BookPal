3f34f26f0e98eb9cb5e2f05b53de30c0b0e8292b
Nikita Vaghela <nikita.vaghela@bistasolutions.com>
2023-01-20 18:19:16 +0530

    [IMP]Updated version of bista_bigcommerce_extend module just fix the error.
    (cherry picked from commit bea667d6e537fa7c67beb6e260e6f0ecff978ff3)

--- bista_bigcommerce_extend/__manifest__.py
+++ bista_bigcommerce_extend/__manifest__.py
@@ -4,7 +4,7 @@
     'category': 'integration',
     'author': "Bista Solutions Pvt Ltd.",
     'website': "http://www.bistasolutions.com",
-    'version': '15.0.2.2.1',
+    'version': '15.0.2.3.1',
     'summary': """""",
     'description': """
         Update the bigcommerce functionality.
