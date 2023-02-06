933f26596bd1d3386542553ea99fcaa23e00f5da
Nikita Vaghela <nikita.vaghela@bistasolutions.com>
2023-02-06 16:49:28 +0530

    [ADD]Added bista_avatax_exemption to fix Feature #19153.
    (cherry picked from commit 642d353ab96434de3b7e828316d2e203c87f2335)

---
+++ bista_avatax_exemption/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Bista Tax Exemption",
+    "version": "15.0.1.0.0",
+    "description": "manage tax exemption functionality",
+    "category": "Account",
+    "website": "http://www.bistasolutions.com",
+    "license": "AGPL-3",
+    "depends": ["account_avatax_sale"],
+    "data": [
+        "views/res_company_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
