1deb6d98649c534c8af70d613f6778ae746f93fc
Unnati Bhavsar <unnati.bhavsar@bista-it-00241.bistasolutions.com>
2023-02-12 00:02:46 +0530

    [FIX] - Bug #18939 - update the logic of set the list price of product on vendor price list

--- purchase_pricelist/models/product_supplierinfo.py
+++ purchase_pricelist/models/product_supplierinfo.py
@@ -8,10 +8,10 @@ class ProductSupplierInfo(models.Model):
 
     vendor_pricelist_id = fields.Many2one("product.pricelist", string="Pricelist")
 
-    @api.onchange('name')
-    def _onchange_name(self):
-        super(ProductSupplierInfo, self)._onchange_name()
+    def write(self, vals):
+        res = super(ProductSupplierInfo, self).write(vals)
         if self.product_id:
             self.price = self.product_id.list_price
         elif self.product_tmpl_id:
             self.price = self.product_tmpl_id.list_price
+        return res
