62daf9e79e503cdb4042986af7f59465648bf198
Mukul Roy <mukul.roy@bistasolutions.com>
2023-02-28 19:21:50 +0530

    [IMP] Feature #19644 - Added functionality to calculate shipping cost based on vendor location to customer location

--- bista_sale_multi_ship/__manifest__.py
+++ bista_sale_multi_ship/__manifest__.py
@@ -7,40 +7,38 @@
 ##############################################################################
 
 {
-    'name': 'Sale Multi Ship',
-    'description': """
+    "name": "Sale Multi Ship",
+    "description": """
         Sales Multi Ship
     """,
-    'author': 'Bista Solutions Pvt. Ltd.',
-    'website': 'http://www.bistasolutions.com',
-    'version': '15.0.1.1',
-
+    "author": "Bista Solutions Pvt. Ltd.",
+    "website": "http://www.bistasolutions.com",
+    "version": "15.0.1.1",
     # Dependent module required for this module to be installed
-    'depends': ['bista_sales_approval',
-                'delivery_ups',
-                'contacts',
-                'bista_report_header_footer',
-                ],
-
-    # always loaded
-    'data': [
-        'data/email_data.xml',
-        'data/res_config_data.xml',
-        'views/res_config_settings_views.xml',
-        'security/ir.model.access.csv',
-        'security/product_data.xml',
-        'wizard/put_in_pack_wizard_view.xml',
-        'wizard/external_shipping_wizard_view.xml',
-        'views/res_partner_view.xml',
-        'views/sale_multi_ship.xml',
-        'views/sale_order_view.xml',
-        'views/stock_view.xml',
-        'report/shipment_report_view.xml',
-        'report/report_register.xml',
-
-
+    "depends": [
+        "bista_sales_approval",
+        "delivery_ups",
+        "contacts",
+        "bista_report_header_footer",
+        "stock_dropshipping",
     ],
-    'installable': True,
-    'auto_install': False,
-    'license': 'LGPL-3',
+    # always loaded
+    "data": [
+        "data/email_data.xml",
+        "data/res_config_data.xml",
+        "views/res_config_settings_views.xml",
+        "security/ir.model.access.csv",
+        "security/product_data.xml",
+        "wizard/put_in_pack_wizard_view.xml",
+        "wizard/external_shipping_wizard_view.xml",
+        "views/res_partner_view.xml",
+        "views/sale_multi_ship.xml",
+        "views/sale_order_view.xml",
+        "views/stock_view.xml",
+        "report/shipment_report_view.xml",
+        "report/report_register.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
 }
--- bista_sale_multi_ship/models/delivery_ups.py
+++ bista_sale_multi_ship/models/delivery_ups.py
@@ -1,226 +1,207 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Bista Solutions Pvt. Ltd
-#    Copyright (C) 2022 (http://www.bistasolutions.com)
-#
-##############################################################################
 
 from odoo import models, fields, _
-from odoo.exceptions import UserError
-from odoo.tools import pdf
 
-from odoo.addons.delivery_ups.models.ups_request import UPSRequest, Package
+from odoo.addons.delivery_ups.models.ups_request import Package
+from .ups_request import UPSRequest
 
 
 class ProviderUPS(models.Model):
-    _inherit = 'delivery.carrier'
+    _inherit = "delivery.carrier"
 
-    def ups_send_shipping(self, pickings):
-        res = []
+    def ups_rate_shipment(self, order):
         superself = self.sudo()
-        srm = UPSRequest(self.log_xml, superself.ups_username, superself.ups_passwd,
-                         superself.ups_shipper_number, superself.ups_access_number, self.prod_environment)
-        ResCurrency = self.env['res.currency']
-        for picking in pickings:
+        srm = UPSRequest(
+            self.log_xml,
+            superself.ups_username,
+            superself.ups_passwd,
+            superself.ups_shipper_number,
+            superself.ups_access_number,
+            self.prod_environment,
+        )
+        ResCurrency = self.env["res.currency"]
+        max_weight = self.ups_default_package_type_id.max_weight
+        dropship_id = self.env.ref("stock_dropshipping.route_drop_shipping")
+        dropship_line = order.order_line.filtered(lambda l: l.route_id == dropship_id)
+        non_dropship_line = order.order_line.filtered(
+            lambda l: l.route_id != dropship_id
+        )
+        total_price = 0
+        if non_dropship_line:
             packages = []
-            package_names = []
-            if picking.package_ids:
-                # Create all packages
-                for package in picking.package_ids:
-                    packages.append(Package(
-                        self, package.shipping_weight, quant_pack=package.package_type_id, name=package.name))
-                    package_names.append(package.name)
-            # Create one package with the rest (the content that is not in a
-            # package)
-            if picking.weight_bulk:
-                packages.append(Package(self, picking.weight_bulk))
+            total_qty = 0
+            total_weight = 0
+            for line in non_dropship_line.filtered(
+                lambda l: l.product_id.type in ["product", "consu"]
+                and not l.is_delivery
+                and not l.display_type
+            ):
+                total_weight += line.product_qty * line.product_id.weight
+            for line in non_dropship_line.filtered(
+                lambda line: not line.is_delivery and not line.display_type
+            ):
+                total_qty += line.product_uom_qty
 
-            shipment_info = {
-                'description': picking.origin,
-                'total_qty': sum(sml.qty_done for sml in picking.move_line_ids),
-                'ilt_monetary_value': '%d' % sum(sml.sale_price for sml in picking.move_line_ids),
-                'itl_currency_code': self.env.company.currency_id.name,
-                'phone': picking.partner_id.mobile or picking.partner_id.phone or picking.sale_id.partner_id.mobile or picking.sale_id.partner_id.phone,
-            }
-            if picking.sale_id and picking.sale_id.carrier_id != picking.carrier_id:
-                ups_service_type = picking.carrier_id.ups_default_service_type or self.ups_default_service_type
+            if max_weight and total_weight > max_weight:
+                total_package = int(total_weight / max_weight)
+                last_package_weight = total_weight % max_weight
+
+                for seq in range(total_package):
+                    packages.append(Package(self, max_weight))
+                if last_package_weight:
+                    packages.append(Package(self, last_package_weight))
             else:
-                ups_service_type = self.ups_default_service_type
-            ups_carrier_account = False
-            if self.ups_bill_my_account:
-                # custom code for set third_party_account start
-                ups_carrier_account = picking.sale_id.third_party_shipper
-                # custom code for set third_party_account end
-            if picking.carrier_id.ups_cod:
+                packages.append(Package(self, total_weight))
+
+            # required when service type = 'UPS Worldwide Express Freight'
+            shipment_info = {"total_qty": total_qty}
+
+            if self.ups_cod:
                 cod_info = {
-                    'currency': picking.partner_id.country_id.currency_id.name,
-                    'monetary_value': picking.sale_id.amount_total,
-                    'funds_code': self.ups_cod_funds_code,
+                    "currency": order.partner_id.country_id.currency_id.name,
+                    "monetary_value": order.amount_total,
+                    "funds_code": self.ups_cod_funds_code,
                 }
             else:
                 cod_info = None
 
             check_value = srm.check_required_value(
-                picking.company_id.partner_id, picking.picking_type_id.warehouse_id.partner_id, picking.partner_id, picking=picking)
+                order.company_id.partner_id,
+                order.warehouse_id.partner_id,
+                order.partner_shipping_id,
+                order=order,
+            )
             if check_value:
-                raise UserError(check_value)
+                return {
+                    "success": False,
+                    "price": 0.0,
+                    "error_message": check_value,
+                    "warning_message": False,
+                }
 
-            package_type = picking.package_ids and picking.package_ids[
-                0].package_type_id.shipper_package_code or self.ups_default_package_type_id.shipper_package_code
-            srm.send_shipping(
-                shipment_info=shipment_info, packages=packages, shipper=picking.company_id.partner_id, ship_from=picking.picking_type_id.warehouse_id.partner_id,
-                ship_to=picking.partner_id, packaging_type=package_type, service_type=ups_service_type, duty_payment=picking.carrier_id.ups_duty_payment,
-                label_file_type=self.ups_label_file_type, ups_carrier_account=ups_carrier_account, saturday_delivery=picking.carrier_id.ups_saturday_delivery,
-                cod_info=cod_info)
-            result = srm.process_shipment()
-            if result.get('error_message'):
-                raise UserError(result['error_message'].__str__())
+            ups_service_type = self.ups_default_service_type
+            result = srm.get_shipping_price(
+                shipment_info=shipment_info,
+                packages=packages,
+                shipper=order.company_id.partner_id,
+                ship_from=order.warehouse_id.partner_id,
+                ship_to=order.partner_shipping_id,
+                packaging_type=self.ups_default_package_type_id.shipper_package_code,
+                service_type=ups_service_type,
+                saturday_delivery=self.ups_saturday_delivery,
+                cod_info=cod_info,
+            )
 
-            order = picking.sale_id
-            company = order.company_id or picking.company_id or self.env.company
-            currency_order = picking.sale_id.currency_id
-            if not currency_order:
-                currency_order = picking.company_id.currency_id
+            if result.get("error_message"):
+                return {
+                    "success": False,
+                    "price": 0.0,
+                    "error_message": _("Error:\n%s", result["error_message"]),
+                    "warning_message": False,
+                }
 
-            if currency_order.name == result['currency_code']:
-                price = float(result['price'])
+            if order.currency_id.name == result["currency_code"]:
+                price = float(result["price"])
             else:
                 quote_currency = ResCurrency.search(
-                    [('name', '=', result['currency_code'])], limit=1)
+                    [("name", "=", result["currency_code"])], limit=1
+                )
                 price = quote_currency._convert(
-                    float(result['price']), currency_order, company, order.date_order or fields.Date.today())
+                    float(result["price"]),
+                    order.currency_id,
+                    order.company_id,
+                    order.date_order or fields.Date.today(),
+                )
 
-            package_labels = []
-            for track_number, label_binary_data in result.get('label_binary_data').items():
-                package_labels = package_labels + \
-                    [(track_number, label_binary_data)]
+            if self.ups_bill_my_account and order.partner_ups_carrier_account:
+                # Don't show delivery amount, if ups bill my account option is true
+                price = 0.0
+            total_price = price
 
-            carrier_tracking_ref = "+".join([pl[0] for pl in package_labels])
-            logmessage = _("Shipment created into UPS<br/>"
-                           "<b>Tracking Numbers:</b> %s<br/>"
-                           "<b>Packages:</b> %s") % (carrier_tracking_ref, ','.join(package_names))
-            if self.ups_label_file_type != 'GIF':
-                attachments = [
-                    ('LabelUPS-%s.%s' % (pl[0], self.ups_label_file_type), pl[1]) for pl in package_labels]
-            if self.ups_label_file_type == 'GIF':
-                attachments = [
-                    ('LabelUPS.pdf', pdf.merge_pdf([pl[1] for pl in package_labels]))]
-            if picking.sale_id:
-                for pick in picking.sale_id.picking_ids:
-                    pick.message_post(body=logmessage, attachments=attachments)
+        for line in dropship_line:
+            packages = []
+            total_qty = line.product_uom_qty
+            total_weight = line.product_qty * line.product_id.weight
+
+            if max_weight and total_weight > max_weight:
+                total_package = int(total_weight / max_weight)
+                last_package_weight = total_weight % max_weight
+
+                for seq in range(total_package):
+                    packages.append(Package(self, max_weight))
+                if last_package_weight:
+                    packages.append(Package(self, last_package_weight))
             else:
-                picking.message_post(body=logmessage, attachments=attachments)
-            shipping_data = {
-                'exact_price': price,
-                'tracking_number': carrier_tracking_ref}
-            res = res + [shipping_data]
-            if self.return_label_on_delivery:
-                self.ups_get_return_label(picking)
-        return res
+                packages.append(Package(self, total_weight))
 
-    def ups_get_return_label(self, picking, tracking_number=None, origin_date=None):
-        res = []
-        superself = self.sudo()
-        srm = UPSRequest(self.log_xml, superself.ups_username, superself.ups_passwd,
-                         superself.ups_shipper_number, superself.ups_access_number, self.prod_environment)
-        ResCurrency = self.env['res.currency']
-        packages = []
-        package_names = []
-        if picking.is_return_picking:
-            weight = picking._get_estimated_weight()
-            packages.append(Package(self, weight))
-        else:
-            if picking.package_ids:
-                # Create all packages
-                for package in picking.package_ids:
-                    packages.append(Package(
-                        self, package.shipping_weight, quant_pack=package.package_type_id, name=package.name))
-                    package_names.append(package.name)
-            # Create one package with the rest (the content that is not in a
-            # package)
-            if picking.weight_bulk:
-                packages.append(Package(self, picking.weight_bulk))
+            shipment_info = {"total_qty": total_qty}
 
-        invoice_line_total = 0
-        for move in picking.move_lines:
-            invoice_line_total += picking.company_id.currency_id.round(
-                move.product_id.lst_price * move.product_qty)
+            if self.ups_cod:
+                cod_info = {
+                    "currency": order.partner_id.country_id.currency_id.name,
+                    "monetary_value": order.amount_total,
+                    "funds_code": self.ups_cod_funds_code,
+                }
+            else:
+                cod_info = None
 
-        shipment_info = {
-            'description': picking.origin,
-            'total_qty': sum(sml.qty_done for sml in picking.move_line_ids),
-            'ilt_monetary_value': '%d' % invoice_line_total,
-            'itl_currency_code': self.env.company.currency_id.name,
-            'phone': picking.partner_id.mobile or picking.partner_id.phone or picking.sale_id.partner_id.mobile or picking.sale_id.partner_id.phone,
-        }
-        if picking.sale_id and picking.sale_id.carrier_id != picking.carrier_id:
-            ups_service_type = picking.carrier_id.ups_default_service_type or self.ups_default_service_type
-        else:
+            check_value = srm.check_required_value_vendor(
+                order.company_id.partner_id,
+                line.supplier_id,
+                order.partner_shipping_id,
+                order=order,
+            )
+            if check_value:
+                return {
+                    "success": False,
+                    "price": 0.0,
+                    "error_message": check_value,
+                    "warning_message": False,
+                }
+
             ups_service_type = self.ups_default_service_type
-        ups_carrier_account = False
-        if self.ups_bill_my_account:
-            # custom code for set third_party_account start
-            ups_carrier_account = picking.shipping_partner_id.with_company(
-                picking.company_id).property_ups_carrier_account
-            # custom code for set third_party_account end
-        if picking.carrier_id.ups_cod:
-            cod_info = {
-                'currency': picking.partner_id.country_id.currency_id.name,
-                'monetary_value': picking.sale_id.amount_total,
-                'funds_code': self.ups_cod_funds_code,
-            }
-        else:
-            cod_info = None
+            result = srm.get_shipping_price(
+                shipment_info=shipment_info,
+                packages=packages,
+                shipper=order.company_id.partner_id,
+                ship_from=line.supplier_id,
+                ship_to=order.partner_shipping_id,
+                packaging_type=self.ups_default_package_type_id.shipper_package_code,
+                service_type=ups_service_type,
+                saturday_delivery=self.ups_saturday_delivery,
+                cod_info=cod_info,
+            )
 
-        check_value = srm.check_required_value(
-            picking.partner_id, picking.partner_id, picking.picking_type_id.warehouse_id.partner_id)
-        if check_value:
-            raise UserError(check_value)
+            if result.get("error_message"):
+                return {
+                    "success": False,
+                    "price": 0.0,
+                    "error_message": _("Error:\n%s", result["error_message"]),
+                    "warning_message": False,
+                }
 
-        package_type = picking.package_ids and picking.package_ids[
-            0].package_type_id.shipper_package_code or self.ups_default_package_type_id.shipper_package_code
-        srm.send_shipping(
-            shipment_info=shipment_info, packages=packages, shipper=picking.partner_id, ship_from=picking.partner_id,
-            ship_to=picking.picking_type_id.warehouse_id.partner_id, packaging_type=package_type, service_type=ups_service_type, duty_payment='RECIPIENT', label_file_type=self.ups_label_file_type, ups_carrier_account=ups_carrier_account,
-            saturday_delivery=picking.carrier_id.ups_saturday_delivery, cod_info=cod_info)
-        srm.return_label()
-        result = srm.process_shipment()
-        if result.get('error_message'):
-            raise UserError(result['error_message'].__str__())
+            if order.currency_id.name == result["currency_code"]:
+                price = float(result["price"])
+            else:
+                quote_currency = ResCurrency.search(
+                    [("name", "=", result["currency_code"])], limit=1
+                )
+                price = quote_currency._convert(
+                    float(result["price"]),
+                    order.currency_id,
+                    order.company_id,
+                    order.date_order or fields.Date.today(),
+                )
 
-        order = picking.sale_id
-        company = order.company_id or picking.company_id or self.env.company
-        currency_order = picking.sale_id.currency_id
-        if not currency_order:
-            currency_order = picking.company_id.currency_id
+            if self.ups_bill_my_account and order.partner_ups_carrier_account:
+                # Don't show delivery amount, if ups bill my account option is true
+                price = 0.0
 
-        if currency_order.name == result['currency_code']:
-            price = float(result['price'])
-        else:
-            quote_currency = ResCurrency.search(
-                [('name', '=', result['currency_code'])], limit=1)
-            price = quote_currency._convert(
-                float(result['price']), currency_order, company, order.date_order or fields.Date.today())
+            total_price += price
 
-        package_labels = []
-        for track_number, label_binary_data in result.get('label_binary_data').items():
-            package_labels = package_labels + \
-                [(track_number, label_binary_data)]
-
-        carrier_tracking_ref = "+".join([pl[0] for pl in package_labels])
-        logmessage = _("Return label generated<br/>"
-                       "<b>Tracking Numbers:</b> %s<br/>"
-                       "<b>Packages:</b> %s") % (carrier_tracking_ref, ','.join(package_names))
-        if self.ups_label_file_type != 'GIF':
-            attachments = [('%s-%s-%s.%s' % (self.get_return_label_prefix(), pl[0], index,
-                                             self.ups_label_file_type), pl[1]) for index, pl in enumerate(package_labels)]
-        if self.ups_label_file_type == 'GIF':
-            attachments = [('%s-%s-%s.%s' % (self.get_return_label_prefix(), package_labels[
-                            0][0], 1, 'pdf'), pdf.merge_pdf([pl[1] for pl in package_labels]))]
-        picking.message_post(body=logmessage, attachments=attachments)
-        shipping_data = {
-            'exact_price': price,
-            'tracking_number': carrier_tracking_ref}
-        res = res + [shipping_data]
-        return res
+        return {
+            "success": True,
+            "price": total_price,
+            "error_message": False,
+            "warning_message": False,
+        }
