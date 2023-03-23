e6c8eb841291977bccf43744e8e03c4f64ba2145
Nikita Vaghela <nikita.vaghela@bistasolutions.com>
2023-03-23 21:10:45 +0530

    [FIX]Feature #19927 - fixed the contact issue like  customer, shipping address, shipment contact, primary contact, show address, vendor selection.

--- bista_sale_multi_ship/models/sale_multi_ship.py
+++ bista_sale_multi_ship/models/sale_multi_ship.py
@@ -290,10 +290,10 @@ class SaleMultiShipQtyLines(models.Model):
     product_qty = fields.Float(string="Shipping Qty")
     personalization_1 = fields.Char('Personalization 1')
     personalization_2 = fields.Char('Personalization 2')
-
     partner_id = fields.Many2one(
         'res.partner', string="Shipping Address",
-        ondelete='cascade')
+        ondelete='cascade',
+        default=lambda self: self.get_default_shipping())
     # Fields only for display purpose from SO Line
     name = fields.Char(related='partner_id.name')
     attention = fields.Char(related='partner_id.attention')
