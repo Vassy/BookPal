6936762254022feb2519895178b17cc85c289e3e
Mukul Roy <mukul.roy@bistasolutions.com>
2023-03-28 18:47:34 +0530

    [FIX] Solved traceback when returning IN transfer

--- bista_purchase/wizard/stock_picking_return.py
+++ bista_purchase/wizard/stock_picking_return.py
@@ -6,18 +6,19 @@ from odoo import models
 class ReturnPicking(models.TransientModel):
     _inherit = "stock.return.picking"
 
-    def _create_returns(self):
-        new_picking_id, picking_type_id = super(ReturnPicking, self)._create_returns()
-        # update po line status while creating return
-        if new_picking_id:
-            return_picking = self.env["stock.picking"].browse(new_picking_id)
-            input_location_id = self.env.ref("stock.stock_location_company")
-            vendor_location_id = self.env.ref("stock.stock_location_suppliers")
-            if (
-                return_picking.location_dest_id == vendor_location_id
-                and return_picking.location_id == input_location_id
-            ):
-                return_picking.move_lines.move_orig_ids.mapped(
-                    "purchase_line_id"
-                ).status_id = self.env.ref("bista_purchase.status_line_return").id
-        return new_picking_id, picking_type_id
+    # Commented code for solved traceback when retuning IN transfer
+    # def _create_returns(self):
+    #     new_picking_id, picking_type_id = super(ReturnPicking, self)._create_returns()
+    #     # update po line status while creating return
+    #     if new_picking_id:
+    #         return_picking = self.env["stock.picking"].browse(new_picking_id)
+    #         input_location_id = self.env.ref("stock.stock_location_company")
+    #         vendor_location_id = self.env.ref("stock.stock_location_suppliers")
+    #         if (
+    #             return_picking.location_dest_id == vendor_location_id
+    #             and return_picking.location_id == input_location_id
+    #         ):
+    #             return_picking.move_lines.move_orig_ids.mapped(
+    #                 "purchase_line_id"
+    #             ).status_id = self.env.ref("bista_purchase.status_line_return").id
+    #     return new_picking_id, picking_type_id
