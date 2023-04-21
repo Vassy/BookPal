eabc4e3d4f225a06dda4e8eb0c2a0e6729915eb9
Mukul Roy <mukul.roy@bistasolutions.com>
2023-04-21 17:17:04 +0530

    [IMP] Bug #20945 - set fiscal position false if big commerce order and down payment invoices also update PO tracking email send to customer

--- bista_bigcommerce_extend/models/account_move.py
+++ bista_bigcommerce_extend/models/account_move.py
@@ -1,32 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, models, fields
-
-
-class AccountMove(models.Model):
-    _inherit = "account.move"
-
-    @api.model
-    def create(self, vals):
-        """Remove fiscal position.
-
-        Make blank fiscal position if invoce create from
-        bigcommerce order.
-        """
-        if self._context.get("active_model") == "sale.order":
-            sale_order_id = self.env["sale.order"].browse(
-                self._context.get("active_id")
-            )
-            down_payment = False
-            for line in vals.get('invoice_line_ids', []):
-                if len(line) == 3:
-                    if line[2].get('name', '').startswith('Down payment'):
-                        down_payment = True
-            if down_payment:
-                vals.update({"fiscal_position_id": False})
-            if sale_order_id.big_commerce_order_id:
-                vals.update({"fiscal_position_id": False})
-        return super(AccountMove, self).create(vals)
+from odoo import models, fields
 
 
 class AccountMoveLine(models.Model):
--- bista_bigcommerce_extend/wizard/__init__.py
+++ bista_bigcommerce_extend/wizard/__init__.py
@@ -1,1 +1,4 @@
+# -*- coding: utf-8 -*-
+
 from . import update_partner_wizard
+from . import sale_make_invoice_advance
