d4bb7aa221654593bcd6d83354d7a05989461687
Mukul Roy <mukul.roy@bistasolutions.com>
2023-04-24 16:45:00 +0530

    [IMP] Bug #20946 - calculate taxes based on shipping contact in the invoice

--- bista_avatax_exemption/models/account_avatax.py
+++ bista_avatax_exemption/models/account_avatax.py
@@ -1,47 +1,39 @@
-
 from odoo import fields, models
 
 
 class AccountAvatax(models.AbstractModel):
-    _inherit = 'account.avatax'
+    _inherit = "account.avatax"
 
     def _get_avatax_taxes(self, commit):
-        """Get the transaction values.
-
-        Overide method to calculate tax based on shipping partner.
-        """
+        # Overide method to calculate tax based on shipping partner.
         self.ensure_one()
-
         partner = self.partner_id.commercial_partner_id
-        if self._name == 'sale.order':
+        if self._name in ("sale.order", "account.move") and self.partner_shipping_id:
             partner = self.partner_shipping_id
-        if self._context.get('partner'):
-            partner = self._context.get('partner')
+        if self._context.get("partner"):
+            partner = self._context.get("partner")
         document_date, tax_date = self._get_avatax_dates()
         taxes = {
-            'addresses': self._get_avatax_addresses(
-                self._get_avatax_ship_to_partner()),
-            'companyCode':
-            self.company_id.partner_id.avalara_partner_code or '',
-            'customerCode':
-            partner.avalara_partner_code or partner.avatax_unique_code,
-            'entityUseCode':
-            partner.with_company(self.company_id).avalara_exemption_id.code or
-            '',
-            'businessIdentificationNo': partner.vat or '',
-            'date': (document_date or fields.Date.today()).isoformat(),
-            'lines': self._get_avatax_invoice_lines(),
-            'type': self._get_avatax_document_type(),
-            'code': self.avatax_unique_code,
-            'referenceCode': self._get_avatax_ref(),
-            'currencyCode': self.currency_id.name or '',
-            'commit': commit and self.company_id.avalara_commit,
+            "addresses": self._get_avatax_addresses(self._get_avatax_ship_to_partner()),
+            "companyCode": self.company_id.partner_id.avalara_partner_code or "",
+            "customerCode": partner.avalara_partner_code or partner.avatax_unique_code,
+            "entityUseCode": partner.with_company(
+                self.company_id
+            ).avalara_exemption_id.code
+            or "",
+            "businessIdentificationNo": partner.vat or "",
+            "date": (document_date or fields.Date.today()).isoformat(),
+            "lines": self._get_avatax_invoice_lines(),
+            "type": self._get_avatax_document_type(),
+            "code": self.avatax_unique_code,
+            "referenceCode": self._get_avatax_ref(),
+            "currencyCode": self.currency_id.name or "",
+            "commit": commit and self.company_id.avalara_commit,
         }
-
         if tax_date:
-            taxes['taxOverride'] = {
-                'type': 'taxDate',
-                'reason': 'Manually changed the tax calculation date',
-                'taxDate': tax_date.isoformat(),
+            taxes["taxOverride"] = {
+                "type": "taxDate",
+                "reason": "Manually changed the tax calculation date",
+                "taxDate": tax_date.isoformat(),
             }
         return taxes
